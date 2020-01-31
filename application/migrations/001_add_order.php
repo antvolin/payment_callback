20830c45590e80b07f11076ef80a2d89e86c2bff
antvolin <antvolin@gmail.com>
2020-01-31 21:42:08 +0200

    Created Order_model.

--- application/migrations/001_add_order.php
+++ application/migrations/001_add_order.php
@@ -1,5 +1,4 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Migration_Add_order extends CI_Migration {
@@ -7,23 +6,23 @@ class Migration_Add_order extends CI_Migration {
     public function up()
     {
         $this->dbforge->add_field(array(
-            'order_id' => array(
+            'id' => array(
                 'type' => 'INT',
                 'constraint' => 11,
                 'unsigned' => TRUE,
                 'auto_increment' => TRUE
             ),
-            'order_status' => array(
+            'status' => array(
                 'type' => 'VARCHAR',
                 'constraint' => '10',
             ),
         ));
-        $this->dbforge->add_key('order_id', TRUE);
+        $this->dbforge->add_key('id', TRUE);
         $this->dbforge->create_table('order');
     }
 
     public function down()
     {
-        $this->dbforge->drop_table('blog');
+        $this->dbforge->drop_table('order');
     }
 }
---
+++ application/models/Order_model.php
@@ -0,0 +1,21 @@
+<?php
+
+class Order_model extends CI_Model
+{
+    public string $status;
+
+    /**
+     * @param int $id
+     */
+    public function getOrderById(int $id): void
+    {
+        return $this->db->get('order', array('id' => $id));
+    }
+
+    public function updateOrder(array $data, int $id): void
+    {
+        $this->status = $data['status'];
+
+        $this->db->update('order', $this, array('id' => $id));
+    }
+}
